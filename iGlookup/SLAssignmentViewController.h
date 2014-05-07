e6c54ef2d79c6a94599dc62ade9f2aecb23cc07f
Donny Reynolds <donnywith@gmail.com>
2014-05-07 14:14:15 -0700

    assignment view controller linked

---
+++ iGlookup/SLAssignmentViewController.h
@@ -0,0 +1,13 @@
+//
+//  SLAssignmentViewController.h
+//  iGlookup
+//
+//  Created by Donny Reynolds on 5/7/14.
+//  Copyright (c) 2014 Scouting Legion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAssignmentViewController : UITableViewController
+
+@end
